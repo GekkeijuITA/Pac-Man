928e6dbcf329bd3c468244bdd5b86101bc716a6d
Lorenzo Vaccarecci <gekkeijuita@gmail.com>
2025-05-23 16:24:48 +0200

    Tappa 2

---
+++ Tappa_2/includes/textures.hpp
@@ -0,0 +1,4 @@
+#include <string>
+
+inline const std::string STRAIGHT_LINE_H = "../Tappa_2/resources/tiles/straight-tile-h.jpg";
+inline const std::string ANGLE_0 = "../Tappa_2/resources/tiles/corner-tile-0.jpg";
